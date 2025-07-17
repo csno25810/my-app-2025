c5d798f30991a2c9e5340e3260651d35ff466943
YoshiharaNozomi <csno25810@g.nihon-u.ac.jp>
2025-07-17 12:35:26 +0900

    ホーム画面の削除、カレンダーページの追加

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-
-import Home from './pages/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Calendar from './pages/Calendar';
 import Tasks from './pages/Tasks';
 
 function App() {
   return (
     <Router>
-      <nav style={{ padding: '10px', borderBottom: '1px solid #ccc' }}>
-        <Link to="/" style={{ marginRight: '10px' }}>ホーム</Link>
-        <Link to="/tasks">課題管理</Link>
-      </nav>
+      <div>
+        <nav style={{ marginBottom: '20px' }}>
+          <a href="/" style={{ marginRight: '10px' }}>カレンダー</a>
+          <a href="/tasks">課題管理</a>
+        </nav>
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/tasks" element={<Tasks />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Calendar />} />
+          <Route path="/tasks" element={<Tasks />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
---
+++ src/pages/Calendar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Calendar() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>カレンダーページ</h1>
+      <p>ここにカレンダーを実装します。</p>
+    </div>
+  );
+}
+
+export default Calendar;
